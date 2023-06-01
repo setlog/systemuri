e6e374b3b5f47a1faa5228b7957ca1f75350e0b6
Michael Ko. Gajda <draget@speciesm.net>
2023-06-01 11:09:20 +0200

    Fix placeholder

--- systemuri.go
+++ systemuri.go
@@ -1,6 +1,6 @@
 package systemuri
 
-// RegisterURLHandler registers a custom URL handler for the specified schema and application. argumentsPattern the %u is replaced by the system-specific placeholder
+// RegisterURLHandler registers a custom URL handler for the specified schema and application. argumentsPattern the %s is replaced by the system-specific placeholder
 func RegisterURLHandler(name string, scheme string, applicationPath string, argumentsPattern string) error {
 	return registerURLHandler(name, scheme, applicationPath, argumentsPattern)
 }
